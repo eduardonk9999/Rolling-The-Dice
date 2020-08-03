1653fe3b43c0fc52734ba086154670449109150c
Eduardo <thiago.novak7@gmail.com>
2020-08-03 15:25:08 -0300

    Add, js

--- app.js
+++ app.js
@@ -8,3 +8,28 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer, dice;
+
+socres = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementsById('#score-0').textContent = '0';
+document.getElementsById('#score-1').textContent = '0';
+document.getElementsById('#current-0').textContent = '0';
+document.getElementsById('#current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+    //Random Number
+    var dice = Math.floor(Math.random() + 6) + 1;
+
+    //Display the result
+    var diceDOM = document.querySelector('.dice');
+    dice.style.display = 'block';
+    dice.src = 'dice-' + dice + '.png';
+
+    //
+});
